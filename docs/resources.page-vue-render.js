1cb24a61948a1914a7bfe8b44d22c08f0e2e2f22
damithc <damithc@github>
2021-07-14 01:22:34 +0800

    Site Update. [skip ci]

---
+++ docs/resources.page-vue-render.js
@@ -0,0 +1,34 @@
+
+    var pageVueRenderFn = function anonymous(
+) {
+with(this){return _c('div',{attrs:{"id":"app"}},[_c('header',{attrs:{"fixed":""}},[_c('navbar',{attrs:{"type":"dark"},scopedSlots:_u([{key:"brand",fn:function(){return [_c('a',{staticClass:"navbar-brand",attrs:{"href":"/","title":"SE-EDU Home"}},[_c('span',[_c('span',{staticClass:"fas fa-home",attrs:{"aria-hidden":"true"}}),_v(" "),_c('strong',[_c('strong',[_v("SE-EDU")])])])])]},proxy:true},{key:"right",fn:function(){return [_c('li',{staticClass:"nav-link"},[_c('form',{staticClass:"navbar-form"},[_c('searchbar',{attrs:{"data":searchData,"placeholder":"Search this site","on-hit":searchCallback,"menu-align-right":""}})],1)])]},proxy:true}])},[_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/docs/templates.html"}},[_c('span',[_c('strong',[_v("Project templates")])])])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/docs/resources.html"}},[_c('span',[_c('strong',[_v("Learning resources")])])])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/docs/tools.html"}},[_c('span',[_c('strong',[_v("Tools")])])])]),_v(" "),_c('li',[_c('a',{staticClass:"nav-link",attrs:{"href":"/docs/team.html"}},[_c('span',[_c('strong',[_v("Team")])])])])])],1),_v(" "),_c('div',{attrs:{"id":"flex-body"}},[_c('div',{staticClass:"fixed-header-padding",attrs:{"id":"content-wrapper"}},[_m(0),_v(" "),_m(1),_v(" "),_c('h2',{attrs:{"id":"software-engineering-for-self-directed-learners-book"}},[_c('span',{staticClass:"anchor",attrs:{"id":"software-engineering-for-self-directed-learners-book"}}),_c('thumbnail',{attrs:{"circle":"","background":"#e67300","font-color":"white","size":"50"}},[_c('span',{staticClass:"fas fa-book-open",attrs:{"aria-hidden":"true"}})]),_v(" Software Engineering for Self-Directed Learners "),_m(2),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#software-engineering-for-self-directed-learners-book","onclick":"event.stopPropagation()"}})],1),_v(" "),_m(3),_v(" "),_c('h2',{attrs:{"id":"tutorials-coding-standards-and-best-practices-collection"}},[_c('span',{staticClass:"anchor",attrs:{"id":"tutorials-coding-standards-and-best-practices-collection"}}),_c('thumbnail',{attrs:{"circle":"","background":"#e67300","font-color":"white","size":"50"}},[_c('span',{staticClass:"fas fa-file-alt",attrs:{"aria-hidden":"true"}})]),_v(" Tutorials, coding standards, and best practices "),_m(4),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#tutorials-coding-standards-and-best-practices-collection","onclick":"event.stopPropagation()"}})],1),_v(" "),_m(5),_v(" "),_c('h2',{attrs:{"id":"a-student-s-guide-to-software-engineering-tools-and-amp-techniques-collection"}},[_c('span',{staticClass:"anchor",attrs:{"id":"a-student-s-guide-to-software-engineering-tools-and-amp-techniques-collection"}}),_c('thumbnail',{attrs:{"circle":"","background":"#e67300","font-color":"white","size":"50"}},[_c('span',{staticClass:"fas fa-file-alt",attrs:{"aria-hidden":"true"}})]),_v(" A Student's Guide to Software Engineering Tools & Techniques "),_m(6),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#a-student-s-guide-to-software-engineering-tools-and-amp-techniques-collection","onclick":"event.stopPropagation()"}})],1),_v(" "),_m(7),_v(" "),_c('i',{staticClass:"fa fa-arrow-circle-up fa-lg d-print-none",attrs:{"id":"scroll-top-button","onclick":"handleScrollTop()","aria-hidden":"true"}})])]),_v(" "),_m(8)])}
+};
+    var pageVueStaticRenderFns = [function anonymous(
+) {
+with(this){return _c('h1',{staticClass:"display-4",attrs:{"id":"learning-resources"}},[_c('span',{staticClass:"anchor",attrs:{"id":"learning-resources"}}),_v("Learning resources"),_c('a',{staticClass:"fa fa-anchor",attrs:{"href":"#learning-resources","onclick":"event.stopPropagation()"}})])}
+},function anonymous(
+) {
+with(this){return _c('div',{staticClass:"lead"},[_c('p',[_v("These are learning resources for SE students (e.g., textbooks, tutorials, coding standards, etc.) from SE-EDU.")])])}
+},function anonymous(
+) {
+with(this){return _c('small',[_c('span',{staticClass:"badge badge-pill badge-secondary"},[_v("#book")])])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_v("This is an online SE textbook for SE students.")]),_v(" "),_c('li',[_v("Available at "),_c('strong',[_c('a',{attrs:{"href":"https://se-education.org/se-book"}},[_v("https://se-education.org/se-book")])])])])}
+},function anonymous(
+) {
+with(this){return _c('small',[_c('span',{staticClass:"badge badge-pill badge-secondary"},[_v("#collection")])])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_v("This is a collection of tutorials, coding standards, and best practices suitable for SE students.")]),_v(" "),_c('li',[_v("Available at "),_c('strong',[_c('a',{attrs:{"href":"https://se-education.org/guides"}},[_v("https://se-education.org/guides")])])])])}
+},function anonymous(
+) {
+with(this){return _c('small',[_c('span',{staticClass:"badge badge-pill badge-secondary"},[_v("#collection")])])}
+},function anonymous(
+) {
+with(this){return _c('ul',[_c('li',[_v("This is a collection of articles introducing some tools and techniques relevant to SE student.")]),_v(" "),_c('li',[_v("Available at "),_c('strong',[_c('a',{attrs:{"href":"https://se-education.org/learningresources"}},[_v("https://se-education.org/learningresources")])])])])}
+},function anonymous(
+) {
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("[Powered by "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 3.0.4")]),_v(" on Wed, 14 Jul 2021, 1:22:19 GMT+8]")])])])}
+}];
+  
